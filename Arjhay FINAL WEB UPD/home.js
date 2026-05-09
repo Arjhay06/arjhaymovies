3669dade9367c8e05adb1aef84aa09db3c8585e6
Arjhay06 <nepi.apal.coc@phinmaed.com>
2026-05-09 15:41:50 +0800

    Update home.js

--- Arjhay FINAL WEB UPD/home.js
+++ Arjhay FINAL WEB UPD/home.js
@@ -39,7 +39,7 @@ function getAllowedServers(mediaType, item = {}) {
     return ['vidsrcme.ru','.cc','.in','2Anime','2embed','2embed RU','moviesapi.club animetv','apimocine tv'];
   }
 
-  return ['vidsrcme.ru','player.videasy.net','apimocine tv'];
+  return ['vidsrcme.ru','player.videasy.net','apimocine-tv'];
 }
 
 function getPreferredServer(mediaType, requestedServer, item = {}) {
